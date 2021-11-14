67c63722de6c521aa14d3309f063c024a94d737f
Sundev79 <sundev79@nutdevs.org>
2021-11-14 22:22:55 +0100

    ProjWidget: Write the piece

--- src/ProjWidget.js
+++ src/ProjWidget.js
@@ -2,10 +2,21 @@ import './ProjWidget.scss';
 
 function ProjWidget() {
   return (
-    <div className="main-container">
-      <div className="coming-soon">
-
+    <div className="coming-soon">
+      <div className="projname">
+        <img 
+          className="projname-logo" 
+          alt="keOS-logo" 
+            src="https://raw.githubusercontent.com/KetchupOS/KetchupOS/master/Icons/KetchupOS-Logo.svg" 
+        />
+        
+        <div className="projabout">
+          <span className="proj-title">KetchupOS<sup>(keOS)</sup></span>
+          <span className="proj-desc">Modern. Powerful. Responsive.</span>
+        </div>
       </div>
+
+      <span className="proj-status">Coming soon.</span>
     </div>
   );
 }
